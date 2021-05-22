6b617661b81f7f25e2a11ceb1127ddeaa6342e35
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-05-22 21:58:59 +0530

    Create varlet.ts

---
+++ varlet.ts
@@ -0,0 +1,16 @@
+//var keyword
+var greeter = "hey hi";  
+var times = 5;  
+if (times > 3) {  
+   var greeter = "Say Hello";   
+}  
+console.log(greeter) //Output: Say Hello  
+
+//let keyword
+let greeter = "hey hi";  
+let times = 5;  
+if (times > 3) {  
+   let hello = "Say Hello ";   
+   console.log(hello) // Output: Say Hello 
+}  
+console.log(hello) // Compile error: greeter is not defined  
