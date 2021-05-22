f8efee551f69bc759b62f1c0e9b0412db2043793
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-05-22 22:04:50 +0530

    Create operators.ts

---
+++ operators.ts
@@ -0,0 +1,14 @@
+//opreators
+let a = 20;
+let b = 30;
+let c = a + b; //we can use (-, *, *, /, %)
+console.log( c ); 
+
+//Ternary operator
+let num = 16;  
+let result = (num > 0) ? "True":"False"   
+console.log(result);  //true
+
+//concatenation
+let message = "Welcome to " + "Office";  
+console.log("Result of message is:" +message); //Result of message is Welcom to Office
